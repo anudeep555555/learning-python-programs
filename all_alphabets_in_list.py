0d575c7c9d99482eb8611dec3098fdaabacf5e44
anudeep555555 <64323150+anudeep555555@users.noreply.github.com>
2020-09-20 12:57:43 +0530

    Create all_alphabets_in_list.py

---
+++ all_alphabets_in_list.py
@@ -0,0 +1,7 @@
+listy=[]
+for i in range(97,122+1,+1):
+    listy.append(chr(i))
+print(listy)
+
+#If you want to know what is 97 you can check it by printing the value of  ord(a)
+#ord(a)=97  and chr(97)='a'
